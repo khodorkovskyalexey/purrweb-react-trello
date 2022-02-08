768fa9c8fcff112582de13fee61e4775972bb2ee
Alexey <aezz120813@gmail.com>
2022-02-08 18:28:05 +0600

    feat: exit button

--- src/common/components/Modal/Modal.tsx
+++ src/common/components/Modal/Modal.tsx
@@ -10,6 +10,7 @@ export const Modal: React.FC<ModalProps> = ({ active, setActive, children }) => 
     return (
         <div className={active ? 'modal_view active' : 'modal_view'} onClick={() => setActive(false)}>
             <div className={active ? 'modal_view_content active' : 'modal_view_content'} onClick={e => e.stopPropagation()}>
+                <input type="button" className='fl_r' value='x' onClick={() => setActive(false)} />
                 {children}
             </div>
         </div>
